73eb6d574c24ba25dba4e47fff0004ab65ef3007
David Terrell <dterrell@gmail.com>
2022-05-14 01:07:53 -0700

    Add support for multiple commands
    Split main.rs
    - init.rs is _start and friends
    - bin/hello.rs is old hello world
    - bin/mandelbrot.rs is new mandelbrot text demo
    
    Also add formatting macros to uart.rs
    Also make uart.rs infer \r when necessary
    Also run cargo fmt on everything

--- rust/BUILD.md
+++ rust/BUILD.md
@@ -1,17 +1,19 @@
 all commands assume the rust/ directory is current
 
+Some commands assume you've run `cargo install cargo-binutils`
+
 ## Build
 
 `cargo build --release`
 
 ## Run the build in QEMU
 
-`${QEMU}/build/qemu-system-riscv32 -machine micro -bios ../out/boot.elf -nographic -monitor none -serial stdio -kernel target/riscv32i-unknown-none-elf/release/tinyos`
+`${QEMU}/build/qemu-system-riscv32 -machine micro -bios ../out/boot.elf -nographic -monitor none -serial stdio -kernel target/riscv32i-unknown-none-elf/release/mandelbrot`
 
 ## Generate a binary for the target device
 
-`cargo objcopy --release -- -O binary  ../out/tinyos.bin`
+`cargo objcopy --release --bin mandelbrot -- -O binary  target/mandelbrot.bin`
 
 ## Run on the device
 
-`sconsole -t -b../out/boot.bin@0x40000000 -b../out/tinyos.bin@0x40008000 /dev/ttyACM0 1000000`
+`sconsole -t -b../out/boot.bin@0x40000000 -btarget/mandelbrot.bin@0x40008000 /dev/ttyACM0 1000000`
--- rust/src/uart.rs
+++ rust/src/uart.rs
@@ -2,23 +2,44 @@ const UART0_BASE: *mut u8 = 0xF0002800 as *mut u8;
 const TXFULL: isize = 0x4;
 
 use core::fmt;
+use core::fmt::Write;
 
 use core::ptr::{read_volatile, write_volatile};
 
 pub struct Uart {}
 
 fn putc(addr: *mut u8, v: u8) {
+    if v == b'\n' {
+        putc(addr, b'\r')
+    }
     unsafe {
-	while read_volatile(addr.offset(TXFULL)) > 0 {}
-	write_volatile(addr, v);
+        while read_volatile(addr.offset(TXFULL)) > 0 {}
+        write_volatile(addr, v);
     }
 }
 
 impl fmt::Write for Uart {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-	for ch in s.bytes() {
-	    putc(UART0_BASE, ch);
-	}
-	Ok(())
+        for ch in s.bytes() {
+            putc(UART0_BASE, ch);
+        }
+        Ok(())
     }
+}
+
+pub fn print(arg: fmt::Arguments) {
+    let mut u = Uart {};
+    u.write_fmt(arg).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+	($($arg:tt)*) => ($crate::uart::print(format_args!($($arg)*)))
+}
+
+#[macro_export]
+macro_rules! println {
+	() => (print!("\n"));
+	($arg:expr) => (print!(concat!($arg, "\n")));
+	($fmt:expr, $($arg:tt)*) => (print!(concat($fmt, "\n"), $($arg)*))
 }
