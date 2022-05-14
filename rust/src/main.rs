038b6d7df9bf87940a08973c2e535e35fddfa41a
Joe Ardent <code@ardent.nebcorp.com>
2022-05-13 17:16:20 -0700

    builds and runs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,6 +3,18 @@
 
 use core::panic::PanicInfo;
 
+#[no_mangle]
+#[link_section = "start"]
+pub unsafe extern "C" fn _start() -> ! {
+    let _x = 42;
+
+    let addr = 0xf0002800 as *mut u32;
+    *addr = 0x33;
+
+    // can't return so we go into an infinite loop here
+    loop {}
+}
+
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
     loop {}
