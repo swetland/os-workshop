e54fee7835aa9ce015e593ca73795d3b1c836af8
David Terrell <dterrell@gmail.com>
2022-05-15 13:32:03 -0700

    cargo fmt

--- rust/src/uart.rs
+++ rust/src/uart.rs
@@ -7,27 +7,27 @@ use core::fmt::Write;
 pub struct Uart {}
 
 fn putc(addr: *mut u8, v: u8) {
-	if v == b'\n' {
-		putc(addr, b'\r')
-	}
-	unsafe {
-		while addr.offset(TXFULL).read_volatile() > 0 {}
-		addr.write_volatile(v);
-	}
+    if v == b'\n' {
+        putc(addr, b'\r')
+    }
+    unsafe {
+        while addr.offset(TXFULL).read_volatile() > 0 {}
+        addr.write_volatile(v);
+    }
 }
 
 impl fmt::Write for Uart {
-	fn write_str(&mut self, s: &str) -> fmt::Result {
-		for ch in s.bytes() {
-			putc(UART0_BASE, ch);
-		}
-		Ok(())
-	}
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for ch in s.bytes() {
+            putc(UART0_BASE, ch);
+        }
+        Ok(())
+    }
 }
 
 pub fn print(arg: fmt::Arguments) {
-	let mut u = Uart {};
-	u.write_fmt(arg).unwrap();
+    let mut u = Uart {};
+    u.write_fmt(arg).unwrap();
 }
 
 #[macro_export]
