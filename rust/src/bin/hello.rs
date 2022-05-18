e4204a8338051e827e8ee39daf7087078018d1ba
Joe Ardent <code@ardent.nebcorp.com>
2022-05-18 16:43:37 -0700

    Rust: silence clippy lints.

--- rust/src/bin/hello.rs
+++ rust/src/bin/hello.rs
@@ -1,11 +1,11 @@
 #![no_main]
 #![no_std]
 
-use tinyos::{entry_fn, print, println};
+use tinyos::{entry_fn, spin, print, println};
 
 entry_fn!(safe_start);
 
 fn safe_start() -> ! {
     println!("Hello from Rust!");
-    loop {}
+    spin()
 }
