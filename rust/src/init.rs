85d8fb7a268e770f5c457dd43358214999268ea2
Joe Ardent <code@ardent.nebcorp.com>
2022-05-18 15:45:56 -0700

    Rust: remove old ld scripts, add full start.S to init.

--- rust/src/init.rs
+++ rust/src/init.rs
@@ -1,16 +1,43 @@
-use core::arch::asm;
+use core::arch::global_asm;
 
 use core::panic::PanicInfo;
 
+global_asm!(
+    r#"
+.section ".start"
+
+.globl _start
+_start:
+    /* zero BSS */
+    la t0, __bss_start
+    la t1, __bss_end
+zero_loop:
+    beq t0, t1, zero_done
+    sw zero, 0(t0)
+    add t0, t0, 4
+    j zero_loop
+zero_done:
+
+    /* setup initial stack */
+    la sp, __stack_top
+
+    /* enter C code */
+    jal start
+
+    /* infinite loop */
+    j .
+"#
+);
+
 #[no_mangle]
 #[link_section = ".start"]
-pub unsafe extern "C" fn _start() -> ! {
+pub unsafe extern "C" fn __start() -> ! {
     extern "Rust" {
         fn start() -> !;
     }
 
     // TODO: initialize bss to 0. The BSS segment is currently length zero so ...
-    asm!("la sp, __stack_top", "j start",);
+    // asm!("la sp, __stack_top", "j start",);
     start()
 }
 
