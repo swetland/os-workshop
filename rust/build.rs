356219d8f13503c92c4a76d217979a40f3689c79
Joe Ardent <code@ardent.nebcorp.com>
2022-05-19 14:26:23 -0700

    Rust build: compile start.S directly from C project location.

--- rust/build.rs
+++ rust/build.rs
@@ -1,32 +1,38 @@
 use glob::glob;
 use regex::Regex;
-use std::fs;
-
-// (
-//  echo '// GENERATED FILE, DO NOT EDIT' ;
-//  sed \
-//      -e 's@unsigned char \([^[]*\)\[@pub const \1: [u8; @' \
-//      -e 's@{@[@' \
-//      -e 's@}@]@' \
-//      ../external/vgafonts.c
-// ) > src/external/vgafonts.rs
+use std::{env, fs, path::PathBuf};
 
 fn main() {
-    let src = "../external/vgafonts.c";
-    let dest = "src/external/vgafonts.rs";
-    println!("cargo:rerun-if-changed={}", src);
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let vga_src = "../external/vgafonts.c";
+    println!("cargo:rerun-if-changed={}", vga_src);
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-link-arg=-Tmake/app.ram.ld");
     println!("cargo:rustc-link-arg=-melf32lriscv");
+    // extend the library search path
+    println!("cargo:rustc-link-search={}", out_dir.display());
 
-    let contents = fs::read_to_string(src).unwrap();
+    // copy and transform the FB VGA data
+    let vga_dest = "src/external/vgafonts.rs";
+    let contents = fs::read_to_string(vga_src).unwrap();
     let c1 = Regex::new(r"unsigned char ([^\[]*)\[")
         .unwrap()
         .replace_all(&contents, r"pub const $1: [u8; ");
     let c2 = Regex::new(r"\{").unwrap().replace_all(&c1, r"[");
     let c3 = Regex::new(r"\}").unwrap().replace_all(&c2, r"]");
 
-    fs::write(dest, &*c3).unwrap();
+    fs::write(vga_dest, &*c3).unwrap();
+
+    // asm setup
+    if env::var_os("CC").is_none() {
+        let cc_val = "riscv64-unknown-elf-gcc";
+        let cc_key = "CC";
+        env::set_var(cc_key, cc_val);
+    }
+    cc::Build::new()
+        .file("../hw/src/start.S")
+        .includes(["../hw/inc", "../libc/inc"])
+        .compile("start");
 
     // grab linker scripts
     fs::create_dir("make").unwrap_or(());
@@ -34,7 +40,7 @@ fn main() {
         .expect("could not find .ld files in ../make/")
         .flatten()
     {
-        let dst = format!("make/{}", ld.to_str().unwrap());
+        let dst = format!("make/{}", ld.display());
         fs::copy(ld, dst).unwrap();
     }
 }
--- rust/src/init.rs
+++ rust/src/init.rs
@@ -1,33 +1,4 @@
-use core::arch::global_asm;
-
 use core::panic::PanicInfo;
-
-global_asm!(
-    r#"
-.section ".start"
-
-.globl _start
-_start:
-    /* zero BSS */
-    la t0, __bss_start
-    la t1, __bss_end
-zero_loop:
-    beq t0, t1, zero_done
-    sw zero, 0(t0)
-    add t0, t0, 4
-    j zero_loop
-zero_done:
-
-    /* setup initial stack */
-    la sp, __stack_top
-
-    /* enter C code */
-    jal start
-
-    /* infinite loop */
-    j .
-"#
-);
 
 /// # Safety
 ///
@@ -36,8 +7,7 @@ zero_done:
 /// Likewise, reality is dangerous, and we're talking directly to semi-sentient rock with tiny lightning.
 ///
 #[no_mangle]
-#[link_section = ".start"]
-pub unsafe extern "C" fn __start() -> ! {
+pub unsafe extern "C" fn __mane() -> ! {
     extern "Rust" {
         fn start() -> !;
     }
