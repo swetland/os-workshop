7ace3e47cbaa0b8d79223fbbc697e8103b9a48d1
Joe Ardent <code@ardent.nebcorp.com>
2022-05-22 10:18:50 -0700

    Rust: new beginnings.

---
+++ rust/tiny-rt/platform_bindings.h
@@ -0,0 +1,3 @@
+#include "../../hw/inc/hw/platform.h"
+#include "../../hw/inc/hw/riscv.h"
+#include "../../hw/inc/hw/litex.h"
---
+++ rust/tiny-rt/src/uart.rs
@@ -0,0 +1,43 @@
+const UART0_BASE: *mut u8 = 0xF0002800 as *mut u8;
+const TXFULL: isize = 0x4;
+
+use core::fmt;
+use core::fmt::Write;
+
+pub struct Uart {}
+
+fn putc(addr: *mut u8, v: u8) {
+    if v == b'\n' {
+        putc(addr, b'\r')
+    }
+    unsafe {
+        while addr.offset(TXFULL).read_volatile() > 0 {}
+        addr.write_volatile(v);
+    }
+}
+
+impl fmt::Write for Uart {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for ch in s.bytes() {
+            putc(UART0_BASE, ch);
+        }
+        Ok(())
+    }
+}
+
+pub fn print(arg: fmt::Arguments) {
+    let mut u = Uart {};
+    u.write_fmt(arg).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+	($($arg:tt)*) => ($crate::uart::print(format_args!($($arg)*)))
+}
+
+#[macro_export]
+macro_rules! println {
+	() => (print!("\n"));
+	($arg:expr) => (print!(concat!($arg, "\n")));
+	($fmt:expr, $($arg:tt)*) => (print!(concat($fmt, "\n"), $($arg)*))
+}
